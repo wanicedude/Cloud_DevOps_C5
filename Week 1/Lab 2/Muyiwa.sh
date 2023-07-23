f81fa1d865b95fcf0fa3fcf740f0a63977671a8b
wanicedude <wanicemuyiwa@gmail.com>
2023-07-23 03:27:25 -0700

    Lab1 solution

---
+++ Week 1/Lab 2/Muyiwa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Run this command to make a directory 
+mkdir Muyiwa
+
+#Run this command to create a file
+touch Muyiwa
+
+#Run this command to edit a file using Vim editor and press i to insert edit
+Vi Muyiwa.sh
+
+#Run the command Below to git add the change made to a file
+git add *
+
+#Run the command Below to git commit with a message
+git commit -m "Lab1 solution"
+
+#Run the command Below to git push
+git push
